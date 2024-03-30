911e59aa9a7112a3004cc9ee9b080a8a4897ef39
David Rocendo <rocendod@msu.edu>
2024-03-29 21:21:38 -0400

    Part 1 of Working With Data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+const book = {
+title: 'A Farwell to Ams',
+author: 'Earnest Hemingway',
+published: '1929',
+image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
+width: '264',
+height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      {/* Components can't return multiple JSX tags unless they are wrapped in a parent element */}
+      {/* This is a JSX comment */}
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {/* This a conditional that checks if an image exists before displaying it */}
+      {book.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+      }
+    </div>
+  );
+}
+
+function MagicButton() {
+  return (
+    <div>
+    <h3>This is a Magic Button</h3>
+    <button>Magic</button>
+    </div>
+  );
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>My Favorite Books</h1>
+        <Bookshelf />
       </header>
     </div>
   );
